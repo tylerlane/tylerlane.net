05f0d3c108fa061c7fc7ec65d23476a574dae48c
Tyler Lane <tyler@nolongervalid.com>
2013-09-07 15:44:06 -0500

    adding a model and putting it in the db

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    slug = models.SlugField()
+    active = models.BooleanField(default=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True,null=True)
+    
+    objects = models.Manager()
+    
+    def __unicode__(self):
+        return u"%s" % self.slug
+    
